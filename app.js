daa561e9888f0604a591ad6754375913ce503f04
Arpan Sahni <sahniarpan62@gmail.com>
2023-02-14 11:00:58 +0530

    commited

--- app.js
+++ app.js
@@ -1176,7 +1176,7 @@ app.get('/logout', function(req, res) {
 });
 
 dpController(app);
-var server = app.listen(8081, function() {
+var server = app.listen(3000, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log(host+" "+port);
